9f5254e896de187df7c1ac918d925e3eff23fa6d
Blackstar00010 <Blackstar00010@users.noreply.github.com>
2020-06-14 02:08:54 +0900

    learn Bootstrap

---
+++ js/go-branch.js
@@ -0,0 +1,15 @@
+function zoom() {
+    var win = window.open('../branches/zoom/zooms.html', '_self');
+}
+function purplecomet() {
+    var win = window.open('../branches/PurpleComet/purplecometMain.html', '_self');
+}
+function ahoy() {
+    var win = window.open('../branches/getAhoy/home.html', '_self');
+}
+function learnhtml() {
+    var win = window.open('../branches/LearnHTML1/start.html', '_self');
+}
+function realahoy() {
+    var win = window.open('https://getahoy.net', '_self');
+}
